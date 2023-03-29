f0646474545ae57afb51a7ef78f1ae9fab79a2df
Benjamin McGroarty <bensworkmac@benjamins-mbp-6.mynetworksettings.com>
2023-03-29 09:47:05 -0400

    initial commit of wiki

---
+++ encyclopedia/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django import forms
+from . import util
+
+class SearchForm(forms.Form):
+    search_entry = forms.CharField(label='Search Encyclopedia')
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+    
+def full_entry(request, entry):
+    if util.get_entry(entry):
+        return render(request, "encyclopedia/full_entry.html", {
+            "entry": util.get_entry(entry),
+            "entry_name": entry
+        })
+    else:
+        return render(request, "encyclopedia/apology.html", {
+            "entry_name": entry
+        })
+        
+def search_result(request):
+    if util.get_entry(request):
+        return render()
+    
+
