e3a16c9f8afea66a8161cc4672cc43a56ef601db
Ben McGroarty <benjamin.mcgr@gmail.com>
2023-05-11 13:35:38 -0400

    still working on getting this to save the file to the correct folder.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render
 from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+import os
 
 from . import util
 
 class NewEntryForm(forms.Form):
     title = forms.CharField(label='Entry Title', max_length=80)
-#    entry = forms.CharField(label='Full Entry' widget=forms.Textarea)
+    text = forms.CharField(label='Full Entry', widget=forms.Textarea)
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-    
+
 def full_entry(request, entry):
     if util.get_entry(entry):
         return render(request, "encyclopedia/full_entry.html", {
@@ -25,7 +26,7 @@ def full_entry(request, entry):
         return render(request, "encyclopedia/apology.html", {
             "entry_name": entry
         })
-        
+
 def search_entry(request):
     search = request.GET.get("q")
     #print("entry")
@@ -36,20 +37,23 @@ def search_entry(request):
             "search": search,
             "entries": util.list_entries()
         })
-        
+
 def new_entry(request):
     if request.method == 'POST':
         form = NewEntryForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/thanks/')
-        else:
-            form = NewEntryForm()
-            
-        return render(request, 'encyclopedia/new_entry.html', {'form': form})    
-    
+            title = form.cleaned_data['title']
+            text = form.cleaned_data['text']
+            filename = title.lower().replace(' ', '-') + '.md'
+            if not os.path.isfile(filename):
+                with open(filename, 'w') as f:
+                    f.write('# {}\n\n{}'.format(title, text))
+                    return HttpResponseRedirect(reverse('full_entry', args=(filename,)))
+            else:
+                return render(request, "encyclopedia/apology.html", {
+                    "filename": filename
+            })
 
-    
-    
-
-    
-
+    if request.method == 'GET':
+        form = NewEntryForm()
+        return render(request, 'encyclopedia/new_entry.html', {'form': form})
